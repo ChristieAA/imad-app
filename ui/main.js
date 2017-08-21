76d349429363f253e6498b2d08f3f7887ee242a9
Christie Amalraj <30664532+ChristieAA@users.noreply.github.com>
2017-08-21 12:27:47 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,3 @@
-console.log('Loaded!');
-
-// Move the image
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
-};
 
 // Counter code
 var button = document.getElementById('counter');
